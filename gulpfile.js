31c534b67940b4a2d70c1c1882b0d7b04bd5d3c0
Vishnu B <vishnu.nxg@gmail.com>
2015-05-29 17:47:58 +0530

    adds font-awesome, updates gulpfile, includes image grid

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ var elixir = require('laravel-elixir');
 
 var paths = {
   'jquery': './resources/vendor/bower_components/jquery/',
-  'bootstrap': './resources/vendor/bower_components/bootstrap-sass-official/assets/'
+  'bootstrap': './resources/vendor/bower_components/bootstrap-sass-official/assets/',
+  'font_awesome': './resources/vendor/bower_components/font-awesome/'
 };
 
 elixir(function (mix) {
   mix.sass("main.scss",
            "public/css",
-           {includePaths: [paths.bootstrap + 'stylesheets/']}
+           {includePaths: [paths.bootstrap + 'stylesheets/',
+                           paths.font_awesome + 'scss/'
+            ]}
      )
-    .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
+    .copy(paths.font_awesome + 'fonts/**', 'public/fonts')
     .scripts([
       paths.jquery + "dist/jquery.js",
       paths.bootstrap + "javascripts/bootstrap.js"
